d3074870de146e2a5f835c9b3cb186cd0f299f08
Sridhar Jammalamadaka <sriavr@gmail.com>
2017-08-13 14:00:41 +0530

    added avatar code

---
+++ src/main/java/com/sridhar/configuration/com/sridhar/model/Avatar.java
@@ -0,0 +1,13 @@
+package com.sridhar.configuration.com.sridhar.model;
+
+public interface Avatar {
+
+    public void paritranayaSaadhunaam();
+
+    public void vinashayachaDushkrutam();
+
+    public void dharmaSamsthapanam();
+
+    public void sambhavaamiYugeYuge();
+
+}
---
+++ src/main/java/com/sridhar/configuration/com/sridhar/model/KrishnaAvatar.java
@@ -0,0 +1,23 @@
+package com.sridhar.configuration.com.sridhar.model;
+
+public class KrishnaAvatar implements Avatar {
+    @Override
+    public void paritranayaSaadhunaam() {
+
+    }
+
+    @Override
+    public void vinashayachaDushkrutam() {
+
+    }
+
+    @Override
+    public void dharmaSamsthapanam() {
+
+    }
+
+    @Override
+    public void sambhavaamiYugeYuge() {
+
+    }
+}
